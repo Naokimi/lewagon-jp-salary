e71d0332e1d86dec692b47b41817592f723b7804
Paulo DAlberti <phdalberti@gmail.com>
2022-03-22 10:57:39 +0900

    Allow blank email

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
 
+  validates :email, uniqueness: { allow_blank: true }
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.password = Devise.friendly_token[0, 20]
